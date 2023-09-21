79db69c84d010cd32fef2c1aa5504884553940ca
Abhishek Jaiswal <abhishekjaiswal@Abhisheks-MacBook-Pro.local>
2023-09-21 15:53:15 +0530

    updated script to add OPD_Lite

--- src/main/java/com/tester/localtester/services/Xtest_modified_PROD.java
+++ src/main/java/com/tester/localtester/services/Xtest_modified_PROD.java
@@ -10,14 +10,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Xtest {
+public class Xtest_modified_PROD {
 
     public static void main(String[] args) throws IOException {
 
         Map<String, String> mailData = new HashMap<>();
 
-        String fileDirectory = "/Users/abhishekjaiswal/Desktop/Turtlemint/dp-email-service/";
-        Reader csvFile = new FileReader(fileDirectory + "wellness-2.csv");
+        String fileDirectory = "/Users/abhishekjaiswal/Downloads/";
+        Reader csvFile = new FileReader(fileDirectory + "wellness-3.csv");
 
         String[] HEADERS = {
                 "Pincode",
@@ -29,7 +29,8 @@ public class Xtest {
                 "OPD_Hero",
                 "OPD_Plus",
                 "OPD_Premium",
-                "OPD_Elite"
+                "OPD_Elite",
+                "OPD_Lite"
         };
 
         final String digitalPlanConstant = "digital_Plan";
@@ -41,6 +42,7 @@ public class Xtest {
         final String svaasOPDPlusConstant = "OPD_Plus";
         final String svaasOPDPremiumConstant = "OPD_Premium";
         final String svaasOPDEliteConstant = "OPD_Elite";
+        final String svaasOPDLiteConstant = "OPD_Lite";
 
         final String eKinCare = "ekinCare";
         final String healthAssure = "healthAssure";
@@ -59,7 +61,7 @@ public class Xtest {
         List<XtestModel> lists = new ArrayList<>();
 
         System.out.println("use turtlefin");
-        System.out.println("db.wellnessTestXYZ.insertMany([");
+        System.out.println("wellnessPlansList.update(");
 
         for (CSVRecord record : records) {
             XtestModel model = new XtestModel();
@@ -69,11 +71,11 @@ public class Xtest {
             String actiHealthPlusPlan = record.get("Acti_Health_Plus").trim();
             String healthAssureOnePlan = record.get("Health_Assure_One").trim();
             String healthAssureOnePlusPlan = record.get("Health_Assure_One_Plus").trim();
-
             String opdHero = record.get(svaasOPDHeroConstant).trim();
             String opdPlus = record.get(svaasOPDPlusConstant).trim();
             String opdPremium = record.get(svaasOPDPremiumConstant).trim();
             String opdElite = record.get(svaasOPDEliteConstant).trim();
+            String opdLite = record.get(svaasOPDLiteConstant).trim();
 
             if (pincode.equals("Pincode")) continue;
 
@@ -87,6 +89,7 @@ public class Xtest {
             if (opdPlus.length() > 0) svaasItems.put(svaasOPDPlusConstant, true);
             if (opdPremium.length() > 0) svaasItems.put(svaasOPDPremiumConstant, true);
             if (opdElite.length() > 0) svaasItems.put(svaasOPDEliteConstant, true);
+            if (opdLite.length() > 0) svaasItems.put(svaasOPDLiteConstant, true);
 
             plans.put(eKinCare, ekinCareItems);
             plans.put(healthAssure, healthAssureItems);
@@ -101,24 +104,20 @@ public class Xtest {
 
             lists.add(model);
         }
-
-//        for (var val : lists) {
-//            System.out.println("{\"pincode\":\"" + val.getPincode() + "\", \"plans\":" + val.getPlans()+"},");
-//        }
         System.out.println("]);");
 
-        File tempFile = new File(fileDirectory + "tempfile.json");
+        File tempFile = new File(fileDirectory + "OUTPUT_PROD.json");
         PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
         String line = null;
-        pw.println("use turtlefin;");
-        pw.println("db.wellnessTestXYZ.insertMany([");
+        pw.println("use turtlefin");
 
         for (var val : lists) {
-            line = "{\"pincode\":\"" + val.getPincode() + "\", \"plans\":" + val.getPlans() + "},";
+            pw.println("db.wellnessPlansList.update(");
+            line = "{\"pincode\":\"" + val.getPincode() + "\"},{ $set:" + val.getPlans()+ "}";
             line = line.replace("=", ":");
-            line = line.replace("ekinCare", "\"ekinCare\"");
-            line = line.replace("svaas", "\"svaas\"");
-            line = line.replace("healthAssure", "\"healthAssure\"");
+            line = line.replace("ekinCare", "\"plans.ekinCare\"");
+            line = line.replace("svaas", "\"plans.svaas\"");
+            line = line.replace("healthAssure", "\"plans.healthAssure\"");
 
             line = line.replace("acti_Health_Plus", "\"acti_Health_Plus\"");
             line = line.replace("acti_Health:", "\"acti_Health\":");
@@ -127,14 +126,14 @@ public class Xtest {
             line = line.replace("health_Assure_One:", "\"health_Assure_One\":");
             line = line.replace("health_Assure_One_Plus", "\"health_Assure_One_Plus\"");
 
+            line = line.replace(svaasOPDLiteConstant, "\"opd_Lite\"");
             line = line.replace(svaasOPDEliteConstant, "\"opd_Elite\"");
             line = line.replace(svaasOPDPremiumConstant, "\"opd_Premium\"");
             line = line.replace(svaasOPDPlusConstant, "\"opd_Plus\"");
             line = line.replace(svaasOPDHeroConstant, "\"opd_Hero\"");
             pw.println(line);
+            pw.println(",{upsert: true});");
         }
-        pw.println("]);");
-
         pw.flush();
     }
 }
